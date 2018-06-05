14f6cf203c8fdb2d4c9f95f603d9fa63cac7e116
lorenaquint <lorenaquint@gmail.com>
2018-06-05 15:25:32 -0500

    cmabios onplatform

--- DepServices/DepServices/MainPage.xaml.cs
+++ DepServices/DepServices/MainPage.xaml.cs
@@ -16,7 +16,23 @@ namespace DepServices
 			InitializeComponent();
             this.btnMessage.Clicked += BtnMessage_Clicked;
             this.btnTextoVoz.Clicked += BtnTextoVoz_Clicked;
-		}
+            this.btnPlataforma.Clicked += BtnPlataforma_Clicked;
+            
+        }
+
+        
+
+        private async void BtnPlataforma_Clicked(object sender, EventArgs e)
+        {
+            await Task.Delay(5000).ConfigureAwait(false);
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+             {
+                 Device.BeginInvokeOnMainThread(() =>
+                 this.txtAviso.Text = DateTime.Now.ToString());
+                 return true;
+             });
+            
+        }
 
         private void BtnTextoVoz_Clicked(object sender, EventArgs e)
         {
@@ -32,5 +48,6 @@ namespace DepServices
                           message,
                           "ok");
         }
+        
     }
 }
